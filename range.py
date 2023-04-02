c1751815a522d145bdf0e568d9a0b05622e1b22e
YasiruNilupul <wickramageyn.22@UOM.lk>
2023-04-02 08:03:28 +0530

    Update range.py

--- range.py
+++ range.py
@@ -1,26 +1,14 @@
-first=input().split()
-first=list(map(int, first))
-m=first[1]
-k=first[0]
+from itertools import product
 
+n, m = map(int, input().split())
+lists = []
+for i in range(n):
+    lst = list(map(int, input().split()))[1:]
+    lists.append(lst)
 
+max_sum = 0
+for values in product(*lists):
+    s = sum(v**2 for v in values) % m
+    max_sum = max(max_sum, s)
 
-all_lists=[]
-for i in range(k):
-    list1=input().split()
-    list1=list(map(int, list1))
-    list1.pop(0)
-    all_lists.append(list1[:])
-
-s=0
-high_s=0
-
-for i in all_lists[0]:
-     for j in all_lists[1]:
-         for l in all_lists[2]:
-            s=i**2+j**2+l**2
-            s=s%m
-            if s>high_s:
-                high_s=s
-            s=0
-print(high_s)
+print(max_sum)
